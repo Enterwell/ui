57f202abe6f5ef32063695751ca3f55c5257b081
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-09-12 13:48:38 +0200

    feat: Added component source code section
    Closes #18

--- apps/docs/components/docs/PackageDocs.tsx
+++ apps/docs/components/docs/PackageDocs.tsx
@@ -1,6 +1,4 @@
-import { Card, CardActionArea, Chip } from "@mui/material";
-import Image from "next/image";
-import Link from "next/link";
+import { NpmPackageCard } from "./NpmPackageCard";
 
 type PackageDescriptionProps = {
     metadata: any;
@@ -11,15 +9,9 @@ export function PackageDescription({ metadata }: PackageDescriptionProps) {
         <div className="flex flex-col gap-4">
             <p>{metadata.description}</p>
             <h2 className="text-2xl font-bold">Package</h2>
-            <Card className="max-w-fit rounded-lg border bg-card shadow-xl">
-                <CardActionArea href={`https://www.npmjs.com/package/${metadata.name}`} className="flex items-center gap-2 px-4 py-2">
-                    <div className="flex items-center gap-2">
-                        <Image width={32} height={32} src="/ui/assets/npm.svg" alt="NPM" />
-                        <span>{metadata.name}@{metadata.version}</span>
-                    </div>
-                    <Chip label={metadata.license} />
-                </CardActionArea>
-            </Card>
+            <div className="max-w-fit">
+                <NpmPackageCard name={metadata.name} version={metadata.version} />
+            </div>
         </div>
     )
 }
@@ -30,17 +22,7 @@ export function PackagePeerDependencies({ metadata }: PackageDescriptionProps) {
     return (
         <div className="flex flex-col gap-2">
             {peerDependencyNames.map(name => (
-                <Card className="rounded-lg border bg-card shadow-xl">
-                    <CardActionArea href={`https://www.npmjs.com/package/${metadata.name}`}>
-                        <div className="px-4 py-2 flex items-center justify-between gap-4 w-full">
-                            <span className="flex items-center gap-2">
-                                <Image width={32} height={32} src="/ui/assets/npm.svg" alt="NPM" />
-                                <span>{name}</span>
-                            </span>
-                            <span>{metadata.peerDependencies[name]}</span>
-                        </div>
-                    </CardActionArea>
-                </Card>
+                <NpmPackageCard name={name} version={metadata.peerDependencies[name]} />
             ))}
         </div>
     )
