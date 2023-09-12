da838c9eb10a9c7dd42776802ef9d3975bc2460a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-09-12 13:48:46 +0200

    Leftovers

--- apps/docs/lib/rehype-component.mjs
+++ apps/docs/lib/rehype-component.mjs
@@ -3,18 +3,83 @@ import path from "path"
 import { u } from "unist-builder"
 import { visit } from "unist-util-visit"
 
-function componentSourceFilePath(name) {
+function docsComponentSourceFilePath(name) {
   return `components/${name}.tsx`
 }
 
-function readComponentSourceFiles(name) {
-  // Read the source file.
-  let src = componentSourceFilePath(name);
-  const filePath = path.join(process.cwd(), src)
-  return fs.readFileSync(filePath, "utf8")
+function readComponentSourceFiles(relativePaths) {
+  if (!Array.isArray(relativePaths)) {
+    relativePaths = [relativePaths]
+  }
+
+  // Try to read file from relative paths (first one that exists)
+  for (const relativePath of relativePaths) {
+    try {
+      const filePath = path.join(process.cwd(), relativePath)
+      return { path: relativePath, source: fs.readFileSync(filePath, "utf8") };
+    } catch (error) {
+      // Ignore error
+    }
+  }
+  
+  return null;
 }
 
-function handleComponentSource(node) {
+function highlightCode(source) {
+  if (!source) return;
+
+  // Extract highted sections (when containing "// @highlight-start" and "// @highlight-end" comments")
+  const highlightedRange = [];
+  const codeLines = source.split("\n");
+  let highlightStart = null;
+  for (let i = 0; i < codeLines.length; i++) {
+    const line = codeLines[i];
+    if (line.includes("// @highlight-start")) {
+      highlightStart = i;
+    } else if (line.includes("// @highlight-end")) {
+      highlightedRange.push(`${highlightStart + 1}-${i - 1}`);
+
+      // Remove hightlight comments
+      codeLines.splice(highlightStart, 1);
+      codeLines.splice(i - 1, 1);
+
+      highlightStart = null;
+    }
+  }
+
+  return {
+    codeLines,
+    highlightedRange
+  };
+}
+
+function pushChildCodeBlock(node, codeLines, src, highlightedRange) {
+  node.children?.push(
+    u("element", {
+      tagName: "pre",
+      properties: {
+        __src__: src,
+      },
+      children: [
+        u("element", {
+          tagName: "code",
+          properties: {
+            className: ["language-tsx"],
+          },
+          children: [
+            {
+              type: "text",
+              value: codeLines.join("\n"),
+            },
+          ],
+          data: { meta: `filename="${src}" {${highlightedRange.join(",")}} showLineNumbers` },
+        }),
+      ],
+    })
+  );
+}
+
+function handleComponentWithSource(node) {
   if (node.name === "ComponentWithSource") {
     const component = getNodeAttributeByName(node, "component")?.value;
     const name = component?.value;
@@ -25,54 +90,36 @@ function handleComponentSource(node) {
 
     try {
       // Read the source file.
-      const src = componentSourceFilePath(name);
-      const source = readComponentSourceFiles(name);
+      const src = docsComponentSourceFilePath(name);
+      const { path, source } = readComponentSourceFiles(src);
 
-      // Extract highted sections (when containing "// @highlight-start" and "// @highlight-end" comments")
-      const highlightedRange = [];
-      const codeLines = source.split("\n");
-      let highlightStart = null;
-      for (let i = 0; i < codeLines.length; i++) {
-        const line = codeLines[i];
-        if (line.includes("// @highlight-start")) {
-          highlightStart = i;
-        } else if (line.includes("// @highlight-end")) {
-          highlightedRange.push(`${highlightStart + 1}-${i - 1}`);
-          
-          // Remove hightlight comments
-          codeLines.splice(highlightStart, 1);
-          codeLines.splice(i - 1, 1);
+      const { codeLines, highlightedRange } = highlightCode(source);
 
-          highlightStart = null;
-        }
-      }
+      pushChildCodeBlock(node, codeLines, path.replace("../..", ""), highlightedRange);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
 
-      console.log('ranges', highlightedRange)
+function handleComponentSource(node) {
+  if (node.name === "ComponentSource") {
+    const name = getNodeAttributeByName(node, "name")?.value;
+    const directory = getNodeAttributeByName(node, "directory")?.value;
+    const importPackageName = getNodeAttributeByName(node, "package")?.value;
 
-      // Add code as children so that rehype can take over at build time.
-      node.children?.push(
-        u("element", {
-          tagName: "pre",
-          properties: {
-            __src__: src,
-          },
-          children: [
-            u("element", {
-              tagName: "code",
-              properties: {
-                className: ["language-tsx"],
-              },
-              children: [
-                {
-                  type: "text",
-                  value: codeLines.join("\n"),
-                },
-              ],
-              data: { meta: `filename="${src}" {${highlightedRange.join(",")}} showLineNumbers` },
-            }),
-          ],
-        })
-      )
+    // importPackage is prefixed with "@enterwell/" (npm namespace)
+    const packageName = importPackageName?.replace("@enterwell/", "");
+
+    try {
+      // Read the source file
+      const src = `../../packages/${packageName}/${directory}/${name}.ts`;
+      const srcAlt = `../../packages/${packageName}/${directory}/${name}.tsx`;
+      const { path, source } = readComponentSourceFiles([src, srcAlt]);
+
+      const { codeLines, highlightedRange } = highlightCode(source);
+
+      pushChildCodeBlock(node, codeLines, path, highlightedRange);
     } catch (error) {
       console.error(error)
     }
@@ -82,6 +129,7 @@ function handleComponentSource(node) {
 export function rehypeComponent() {
   return async (tree) => {
     visit(tree, (node) => {
+      handleComponentWithSource(node);
       handleComponentSource(node);
     })
   }
